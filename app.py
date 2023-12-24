da4757869e86360b943cdd3c583f24eb6c9690a8
Thales Sobral <thales.sobral@gmail.com>
2023-12-24 12:15:48 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import database
+import pandas as pd
+
+st.title('Academia')
+
+with st.expander('Cadastro'):
+    with st.form('Cadastro itens', clear_on_submit=True):
+        item = st.text_input('Item')
+        comentario = st.text_input('Comentário')
+        submitted = st.form_submit_button('Cadastrar item')
+        
+        if submitted:
+            database.inserir(item, comentario)
+    
+dados = database.retorna_dados()
+
+if dados:
+    df_dados = pd.DataFrame(dados)
+    df_dados = df_dados[['comprado', 'key', 'comentario']]
+
+    dados_editados = st.data_editor(df_dados)
+
+    atualizar = st.button('Atualizar dados')
+    if atualizar:
+        for index, value in dados_editados.iterrows():
+            database.inserir(item = value['key'], comentario = value['comentario'], comprado = value['comprado'])
+            
+    apagar = st.button('Limpar lista')
+    if apagar:
+        for index, value in dados_editados.iterrows():
+            database.deleta(item = value['key'])
+        st.rerun()
+
+else:
+    st.write('Lista vazia')
---
+++ database.py
@@ -0,0 +1,18 @@
+from deta import Deta
+import streamlit as st
+
+DETA_KEY = st.secrets['DETA_KEY']
+
+deta = Deta(DETA_KEY)
+
+db = deta.Base('academia_app')
+
+def inserir(item, comentario, comprado = False):
+    return db.put({'key': item, 'comentario': comentario, 'comprado': comprado})
+
+def retorna_dados():
+    res = db.fetch()
+    return res.items
+
+def deleta(item):
+    db.delete(item)
